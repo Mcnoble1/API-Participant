2c9dc08000bbbdf3aa6acab8ba16b190567921e3
mcnoble <idowufestustemiloluwa@gmail.com>
2022-07-22 15:22:24 +0100

    reach library and compilation files :octocat:

---
+++ build/index.main.mjs
@@ -0,0 +1,251 @@
+// Automatically generated with Reach 0.1.11 (f1ffa756)
+/* eslint-disable */
+export const _version = '0.1.11';
+export const _versionHash = '0.1.11 (f1ffa756)';
+export const _backendVersion = 17;
+
+export function getExports(s) {
+  const stdlib = s.reachStdlib;
+  return {
+    };
+  };
+export function _getEvents(s) {
+  const stdlib = s.reachStdlib;
+  return {
+    };
+  };
+export function _getViews(s, viewlib) {
+  const stdlib = s.reachStdlib;
+  
+  return {
+    infos: {
+      },
+    views: {
+      }
+    };
+  
+  };
+export function _getMaps(s) {
+  const stdlib = s.reachStdlib;
+  const ctc0 = stdlib.T_Tuple([]);
+  return {
+    mapDataTy: ctc0
+    };
+  };
+export async function Alice(ctcTop, interact) {
+  if (typeof(ctcTop) !== 'object' || ctcTop._initialize === undefined) {
+    return Promise.reject(new Error(`The backend for Alice expects to receive a contract as its first argument.`));}
+  if (typeof(interact) !== 'object') {
+    return Promise.reject(new Error(`The backend for Alice expects to receive an interact object as its second argument.`));}
+  const ctc = ctcTop._initialize();
+  const stdlib = ctc.stdlib;
+  const ctc0 = stdlib.T_Null;
+  
+  
+  stdlib.protect(ctc0, await interact.ready(), {
+    at: './index.rsh:14:19:application',
+    fs: ['at ./index.rsh:13:9:application call to [unknown function] (defined at: ./index.rsh:13:13:function exp)'],
+    msg: 'ready',
+    who: 'Alice'
+    });
+  
+  const txn1 = await (ctc.sendrecv({
+    args: [],
+    evt_cnt: 0,
+    funcNum: 0,
+    lct: stdlib.checkedBigNumberify('./index.rsh:16:5:dot', stdlib.UInt_max, '0'),
+    onlyIf: true,
+    out_tys: [],
+    pay: [stdlib.checkedBigNumberify('./index.rsh:16:5:decimal', stdlib.UInt_max, '0'), []],
+    sim_p: (async (txn1) => {
+      const sim_r = { txns: [], mapRefs: [], maps: [] };
+      let sim_txn_ctr = stdlib.UInt_max;
+      const getSimTokCtr = () => { sim_txn_ctr = sim_txn_ctr.sub(1); return sim_txn_ctr; };
+      
+      
+      const {data: [], secs: v40, time: v39, didSend: v25, from: v38 } = txn1;
+      
+      ;
+      sim_r.txns.push({
+        kind: 'halt',
+        tok: undefined /* Nothing */
+        })
+      sim_r.isHalt = true;
+      
+      return sim_r;
+      }),
+    soloSend: true,
+    timeoutAt: undefined /* mto */,
+    tys: [],
+    waitIfNotPresent: false
+    }));
+  const {data: [], secs: v40, time: v39, didSend: v25, from: v38 } = txn1;
+  ;
+  return;
+  
+  
+  };
+const _ALGO = {
+  ABI: {
+    impure: [],
+    pure: [],
+    sigs: []
+    },
+  appApproval: `BiACAAEmAQAiNQAxGEEAnChkSSJbNQGBCFs1AjYaABdJQQAHIjUEIzUGADYaAhc1BDYaAzYaARciEkSBoI0GiACHIjQBEkQ0BEkiEkw0AhIRRIAEXw2r+rBCAAAxGYEFEkSxIrIBIrIII7IQMgmyCTIKsgezQgAFMRkiEkQoNAEWNAIWUGc0BkEACoAEFR98dTQHULA0AEkjCDIEEkQxFhJEI0MxGSISREL/3yIxNBJEIzE1EkQiMTYSRCIxNxJEIjUBIjUCQv+vNABJSiMINQA4BzIKEkQ4ECMSRDgIEkSJ`,
+  appClear: `Bg==`,
+  companionInfo: null,
+  extraPages: 0,
+  mapDataKeys: 0,
+  mapDataSize: 0,
+  stateKeys: 0,
+  stateSize: 0,
+  unsupported: [],
+  version: 10,
+  warnings: []
+  };
+const _ETH = {
+  ABI: `[
+  {
+    "inputs": [
+      {
+        "components": [
+          {
+            "internalType": "uint256",
+            "name": "time",
+            "type": "uint256"
+          },
+          {
+            "internalType": "bool",
+            "name": "msg",
+            "type": "bool"
+          }
+        ],
+        "internalType": "struct T1",
+        "name": "_a",
+        "type": "tuple"
+      }
+    ],
+    "stateMutability": "payable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "msg",
+        "type": "uint256"
+      }
+    ],
+    "name": "ReachError",
+    "type": "error"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "_who",
+        "type": "address"
+      },
+      {
+        "components": [
+          {
+            "internalType": "uint256",
+            "name": "time",
+            "type": "uint256"
+          },
+          {
+            "internalType": "bool",
+            "name": "msg",
+            "type": "bool"
+          }
+        ],
+        "indexed": false,
+        "internalType": "struct T1",
+        "name": "_a",
+        "type": "tuple"
+      }
+    ],
+    "name": "_reach_e0",
+    "type": "event"
+  },
+  {
+    "stateMutability": "payable",
+    "type": "fallback"
+  },
+  {
+    "inputs": [],
+    "name": "_reachCreationTime",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "_reachCurrentState",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bytes",
+        "name": "",
+        "type": "bytes"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "_reachCurrentTime",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "stateMutability": "payable",
+    "type": "receive"
+  }
+]`,
+  Bytecode: `0x60806040526040516103cf3803806103cf83398101604081905261002291610118565b600080554360035560408051338152825160208083019190915283015115158183015290517faf3102e4a96b239a2811210526ca19adcde1af3e2876a2c9a5886a5a887a2dcb9181900360600190a161007d34156007610099565b60008080556001819055610093906002906100c2565b506101bb565b816100be5760405163100960cb60e01b81526004810182905260240160405180910390fd5b5050565b5080546100ce90610180565b6000825580601f106100de575050565b601f0160209004906000526020600020908101906100fc91906100ff565b50565b5b808211156101145760008155600101610100565b5090565b60006040828403121561012a57600080fd5b604080519081016001600160401b038111828210171561015a57634e487b7160e01b600052604160045260246000fd5b604052825181526020830151801515811461017457600080fd5b60208201529392505050565b600181811c9082168061019457607f821691505b602082108114156101b557634e487b7160e01b600052602260045260246000fd5b50919050565b610205806101ca6000396000f3fe6080604052600436106100355760003560e01c80631e93b0f11461003e5780638323075714610062578063ab53f2c61461007757005b3661003c57005b005b34801561004a57600080fd5b506003545b6040519081526020015b60405180910390f35b34801561006e57600080fd5b5060015461004f565b34801561008357600080fd5b5061008c61009a565b604051610059929190610137565b6000606060005460028080546100af90610194565b80601f01602080910402602001604051908101604052809291908181526020018280546100db90610194565b80156101285780601f106100fd57610100808354040283529160200191610128565b820191906000526020600020905b81548152906001019060200180831161010b57829003601f168201915b50505050509050915091509091565b82815260006020604081840152835180604085015260005b8181101561016b5785810183015185820160600152820161014f565b8181111561017d576000606083870101525b50601f01601f191692909201606001949350505050565b600181811c908216806101a857607f821691505b602082108114156101c957634e487b7160e01b600052602260045260246000fd5b5091905056fea26469706673582212207fa0551a65406619875c7ad0a321c9043e3b3eb010ed8d916b7f990e84b1509364736f6c634300080c0033`,
+  BytecodeLen: 975,
+  Which: `oD`,
+  version: 7,
+  views: {
+    }
+  };
+export const _stateSourceMap = {
+  1: {
+    at: './index.rsh:17:3:after expr stmt',
+    fs: [],
+    msg: null,
+    who: 'Module'
+    }
+  };
+export const _Connectors = {
+  ALGO: _ALGO,
+  ETH: _ETH
+  };
+export const _Participants = {
+  "Alice": Alice
+  };
+export const _APIs = {
+  Bob: {
+    }
+  };
